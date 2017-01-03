4bbc1a5f6763a516d2aa9eb29d14a1d134eaf7be
louisberns <louisuntitled@gmail.com>
2017-01-03 17:57:44 -0200

    End of Data Types, objects declared

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,62 +1,90 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-
-// $("#main").append("Luís Guilherme Berns Silva");
-
-var awesomeThoughts = "I am awesome!"
-
-console.log(awesomeThoughts);
-
-var funThoughts = awesomeThoughts.replace("awesome", "fun");
-
-console.log(funThoughts);
-
-//$("#main").append(funThoughts);
-
-myName = "Louis";
-myRole = "Design Programmer";
-myContact = "(44) 9999-0000";
-myPic = "images/fry.jpg"
-standardWelcomeMessage = "Welcome!";
-
-var formattedName = HTMLheaderName.replace("%data%", myName);
-var formattedRole = HTMLheaderRole.replace("%data%", myRole);
-
-//$("#header").prepend(formattedRole);
-//$("#header").prepend(formattedName);
-
-//var skills = ["awesomeness", "computer", "internet"];
-
 var bio = {
-	"name" : myName,
-	"role" : myRole,
-	"contact info" : myContact,
-	"picture" : myPic,
-	"welcome message" : standardWelcomeMessage
-}
-
-//$("#main").append(bio);
-
-bio["work"] = "web dev";
-bio.education = "Master";
-
-$("#main").append(bio["work"]);
-$("#main").append(bio.education);
-
-
-var cv = {
-	"school" : [
-	{
-		"graduation" : "Administration",
-		"info 2" : "cellphone"
-	},
-	], [
-	{
-		"info 3" : "information"
-	}
+	"info" : [
+		{
+			"name" : "Louis",
+			"age" : "confidential",
+			"email" : "louisuntitled@gmail.com",
+			"phone" : "you do not have access to that information"
+		},
+		{
+			"about" : "a little description of me",
+			"welcome" : "Hello"
+		}
 	]
 }
 
+var education = {
+	"degree" : [
+		{
+			"name" : "Unicesumar",
+			"course" : "Administration",
+			"type" : "bracharel",
+			"year end" : 2017
+		},
+		{
+			"name" : "College",
+			"course" : "High School",
+			"type" : "High School",
+			"year end" : 2013
+		}
+	],
+	"courses" : [
+		{
+			"name" : "softblue",
+			"course" : "programming logic",
+			"type" : "online",
+			"year end" : 2011
+		},
+		{
+			"name" : "udacity",
+			"course" : "JavaScript Syntax",
+			"type" : "online",
+			"year end" : 2017
+		}
+	]
+}
 
+var work = {
+	"actual" : {
+		"name" : "Unimed",
+		"position" : "project assistant",
+		"year start" : 2016,
+		"year end" : "current"
+	},
+	"previous" : [
+		{
+			"name" : "Unimed",
+			"position" : "intentship",
+			"year start" : 2015,
+			"year end" : 2016
+		},
+		{
+			"name" : "Silberseg",
+			"position" : "associated",
+			"year start" : 2011,
+			"year end" : 2015
+		}
+	]
+
+}
+
+var projects = {
+	"Unimed" : [
+		{
+			"name" : "Hackathon",
+			"role" : "Project Manager",
+			"results" : "described results"
+		},
+		{
+			"name" : "GPS",
+			"role" : "Project Manager",
+			"results" : "described results"
+		}
+	],
+	"personal" : {
+		"name" : "Progress Canvas",
+		"role" : "Owner",
+		"results" : "ongoing"
+	}
+}
 

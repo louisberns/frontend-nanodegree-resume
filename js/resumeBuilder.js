19988a5d21c74def6224c3e482b6b2dca19fd345
louisberns <louisuntitled@gmail.com>
2017-01-03 15:26:52 -0200

    New notes and codes, Lesson on Data Types

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,29 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Luís Guilherme Berns Silva");
+// $("#main").append("Luís Guilherme Berns Silva");
+
+var awesomeThoughts = "I am awesome!"
+
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("awesome", "fun");
+
+console.log(funThoughts);
+
+//$("#main").append(funThoughts);
+
+myName = "Louis"
+myRole = "Design Programmer"
+
+var formattedName = HTMLheaderName.replace("%data%", myName);
+var formattedRole = HTMLheaderRole.replace("%data%", myRole);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var skills = ["awesomeness", ]
+
+
+
+
